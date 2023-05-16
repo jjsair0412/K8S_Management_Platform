2699b1ff49a7f76479945f6b82c57422f42aa9f6
jinseong <jjsair0412@naver.com>
2023-05-16 19:00:17 +0900

    project create && modify README

---
+++ api_client/src/main/java/com/api_client/api_client/ApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.api_client.api_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClientApplication.class, args);
+	}
+
+}
---
+++ scg/src/main/java/com/scg/scg/ScgApplication.java
@@ -0,0 +1,13 @@
+package com.scg.scg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScgApplication.class, args);
+	}
+
+}
